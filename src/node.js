1eeb16b742945a918eff75d1f285ce69087db8ea
Mike-Benn <mbenn49@wgu.edu>
2024-04-22 05:01:43 -0400

    Create node object

---
+++ src/node.js
@@ -0,0 +1,38 @@
+export { Node };
+
+
+function Node(val) {
+    let value = val;
+    let left = null;
+    let right = null;
+
+
+    const getValue = () => value;
+
+    const getLeft = () => left;
+
+    const getRight = () => right;
+
+    const setValue = (val) => {
+        value = val;
+    }
+
+    const setLeft = (node) => {
+        left = node;
+    }
+
+    const setRight = (node) => {
+        right = node;
+    }
+
+    return {
+        getValue,
+        getLeft,
+        getRight,
+        setValue,
+        setLeft,
+        setRight
+    }
+
+
+}
