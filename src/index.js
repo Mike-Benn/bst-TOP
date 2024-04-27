76d178a03c3dc7712af9faeaf286302239f0dad8
Mike-Benn <mbenn49@wgu.edu>
2024-04-27 07:55:33 -0400

    comment out code

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ let test = [1 , 7 , 4 , 23 , 8 , 9 , 4 , 3 , 5 , 7 , 9 , 67 , 6345 , 324];
 let tree = Tree(test);
 tree.setRoot(tree.buildTree(tree.getArray()));
 
-tree.prettyPrint(tree.getRoot());
+//tree.prettyPrint(tree.getRoot());
 
 
 
