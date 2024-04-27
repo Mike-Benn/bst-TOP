530869b9923679783829cf7f8e52d4bd2f3330f2
Mike-Benn <mbenn49@wgu.edu>
2024-04-27 06:50:18 -0400

    Add buildTree method,prettyPrint method

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { Tree } from "./tree";
 
 let test = [1 , 7 , 4 , 23 , 8 , 9 , 4 , 3 , 5 , 7 , 9 , 67 , 6345 , 324];
-
 let tree = Tree();
+tree.sortArray(test);
+let root = tree.buildTree(tree.getArray());
+tree.setRoot(root);
+console.log(tree.getArray());
+console.log(tree.getRoot().getLeft().getRight().getLeft().getValue());
+tree.prettyPrint(tree.getRoot());
 
 
-console.log(tree.sortArray(test));
+
+
+
--- src/tree.js
+++ src/tree.js
@@ -3,8 +3,12 @@ export { Tree };
 
 function Tree() {
     let root;
+    let sortedArray;
+    
 
     const getRoot = () => root;
+
+    const getArray = () => sortedArray;
 
     const setRoot = (val) => {
         root = val;
@@ -21,19 +25,54 @@ function Tree() {
             } 
         }
         sorted.sort((a , b) => a - b);
-
+        sortedArray = sorted;
         return sorted;
     }
 
-    const buildTree = (arr) => {
-
+    const getMidIndex = (sorted) => {
+        return Math.floor(sorted.length / 2);
     }
+
+    const buildTree = (sorted) => {
+        if (sorted.length === 0) {
+            return null;
+        }
+
+        let midIndex = getMidIndex(sorted);
+        let leftArray = sorted.slice(0 , midIndex);
+        let rightArray = sorted.slice(midIndex + 1 , sorted.length);
+
+        let rootNode = Node();
+        rootNode.setValue(sorted[midIndex]);
+        rootNode.setLeft(buildTree(leftArray));
+        rootNode.setRight(buildTree(rightArray));
+
+        return rootNode;
+        
+    }
+
+    const prettyPrint = (node, prefix = "", isLeft = true) => {
+        if (node === null) {
+          return;
+        }
+        if (node.getRight() !== null) {
+          prettyPrint(node.getRight(), `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.getLeft() !== null) {
+          prettyPrint(node.getLeft(), `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+      };
+     
 
     
     return {
         getRoot,
+        getArray,
         setRoot,
         sortArray,
         buildTree,
+        prettyPrint,
+
     }
 }
