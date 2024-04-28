eebe036d81b36454e5d5e49819749fe59646fa6f
Mike-Benn <mbenn49@wgu.edu>
2024-04-28 05:58:27 -0400

    Add deleteValue,helper methods:getmin,dupe updater

--- src/tree.js
+++ src/tree.js
@@ -6,6 +6,26 @@ function Tree(arr) {
     let sortedArray = arr;
     let duplicateArray = [];
     
+
+
+    const getRoot = () => root;
+
+    const getSortedArray = () => sortedArray;
+
+    const getDuplicateArray = () => duplicateArray;
+
+    const setRoot = (node) => {
+        root = node;
+    };
+
+
+    // Returns the middle index of an array
+    const getMidIndex = (sorted) => {
+        return Math.floor(sorted.length / 2);
+    }
+
+
+    // Sorts an array in ascending order after removing any duplicate values, sets sortedArray Tree variable with the sorted array
     const sortArray = (arr) => {
         let sorted = [];
         
@@ -20,25 +40,8 @@ function Tree(arr) {
         
     }
 
-    
 
-    const getRoot = () => root;
-
-    const getSortedArray = () => sortedArray;
-
-    const getDuplicateArray = () => duplicateArray;
-
-    const setRoot = (val) => {
-        root = val;
-    };
-
-
-    
-
-    const getMidIndex = (sorted) => {
-        return Math.floor(sorted.length / 2);
-    }
-
+    // Takes an array and builds a Binary Search Tree from the array
     const buildTree = (sorted) => {
         if (sorted.length === 0) {
             return null;
@@ -57,39 +60,108 @@ function Tree(arr) {
         
     }
 
-    const insert = (value) => {
-        let newNode = Node(value);
+    const minValue = (node) => {
+        let lowestNode = node;
+        while (lowestNode.getLeft() !== null) {
+            lowestNode = lowestNode.getLeft();
+        }
+        return lowestNode.getValue();
+    }
 
-        if (duplicateArray[value] === undefined) {
-            let currNode = root;
+    const updateDuplicateArray = (value , str) => {
+        if (str === "+") {
             duplicateArray[value] = 1;
-
-            while (true) {
-                if (value < currNode.getValue()) {
-                    if (currNode.getLeft() !== null) {
-                        currNode = currNode.getLeft();
-                        
-                    } else if (currNode.getLeft() === null) {
-                        currNode.setLeft(newNode);
-                        break;
-
-                    }
-                }
-                
-                if (value > currNode.getValue()) {
-                    if (currNode.getRight() !== null) {
-                        currNode = currNode.getRight();
-                    
-                    } else if (currNode.getRight() === null) {
-                        currNode.setRight(newNode);
-                        break;
-                    }
-                }
-            }
-            
+        } else if (str === "-") {
+            duplicateArray[value] = undefined;
         }
     }
 
+    // Takes a value and inserts a node, if it's not a duplicate, into it's correct position in the tree based on it's value
+    const insertValue = (value) => {
+        let newNode = Node(value);
+
+        if (duplicateArray[value] === undefined) {
+            updateDuplicateArray(value , "+");
+            if (root !== null) {
+                let currNode = root;
+
+                while (true) {
+                    if (value < currNode.getValue()) {
+                        if (currNode.getLeft() !== null) {
+                            currNode = currNode.getLeft();
+                            
+                        } else if (currNode.getLeft() === null) {
+                            currNode.setLeft(newNode);
+                            break;
+
+                        }
+                    } else {
+                        if (value > currNode.getValue()) {
+                            if (currNode.getRight() !== null) {
+                                currNode = currNode.getRight();
+                            
+                            } else if (currNode.getRight() === null) {
+                                currNode.setRight(newNode);
+                                break;
+                            }
+                        }
+                    }
+                }
+            } else {
+                setRoot(newNode);
+            }
+        }
+    }
+        
+    
+
+    const deleteValueHelper = (node , value) => {
+        
+        if (node === null) {
+            return null;
+        }
+        
+        if (value < node.getValue()) {
+            node.setLeft(deleteValueHelper(node.getLeft() , value));
+
+        } else if (value > node.getValue()) {
+            node.setRight(deleteValueHelper(node.getRight() , value));
+
+        } else if (node.getValue() === value) {
+            if (node.getRight() === null) {
+                             
+                return node.getLeft();
+            } else if (node.getLeft() === null) {
+                 
+                return node.getRight();
+
+            } else {
+                node.setValue(minValue(node.getRight()));
+                node.setRight(deleteValueHelper(node.getRight() , node.getValue()));
+                
+
+
+
+            }
+        } 
+
+        
+        return node;
+
+
+    }
+
+    const deleteValue = (value) => {
+        if (duplicateArray[value] !== undefined) {
+            updateDuplicateArray(value , "-");
+            setRoot(deleteValueHelper(getRoot() , value));
+            
+        } else {
+            console.log("That value does not exist within the binary search tree.");
+        }
+    }
+    
+    // Displays tree in console
     const prettyPrint = (node, prefix = "", isLeft = true) => {
         if (node === null) {
             return;
@@ -113,8 +185,11 @@ function Tree(arr) {
         setRoot,
         sortArray,
         buildTree,
-        insert,
+        insertValue,
+        deleteValue,
         prettyPrint,
+        
+
 
     }
 }
